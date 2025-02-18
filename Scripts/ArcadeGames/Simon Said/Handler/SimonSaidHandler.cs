ccd394a85dd202dfbfafb5deb8c14423f12f3bb0
Bekir <aqwwwe73@gmail.com>
2025-02-18 16:33:03 +0300

    Fast food done

---
+++ Scripts/ArcadeGames/Simon Said/Handler/SimonSaidHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonSaidHandler : MonoBehaviour
+{
+    [SerializeField] private List<SpriteRenderer> _directions = new();
+
+
+
+}
